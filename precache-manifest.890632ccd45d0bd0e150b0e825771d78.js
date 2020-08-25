a1c61af2be51dceea88c371da093101141d39460
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-08-25 22:55:38 +0100

    Updates

---
+++ precache-manifest.890632ccd45d0bd0e150b0e825771d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee88e821382be6e51a4d6b051e68ebc",
+    "url": "/interactive-cv/index.html"
+  },
+  {
+    "revision": "387c3d74f0f33f3398bd",
+    "url": "/interactive-cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cff75a8a438be964c37f",
+    "url": "/interactive-cv/static/js/2.c85acd82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/interactive-cv/static/js/2.c85acd82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387c3d74f0f33f3398bd",
+    "url": "/interactive-cv/static/js/main.1664afb7.chunk.js"
+  },
+  {
+    "revision": "bf923ce1f7b1fe245572",
+    "url": "/interactive-cv/static/js/runtime-main.3de8bcca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/interactive-cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
