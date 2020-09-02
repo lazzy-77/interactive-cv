6f8b2f34b2762b4af9f19d48f01e2818c462a0ef
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-09-02 22:36:56 +0100

    basic alyout completed

--- src/App.js
+++ src/App.js
@@ -4,21 +4,49 @@ import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React ghpages
-				</a>
-			</header>
+		<div className='container-padding'>
+			<div className='container'>
+				<div id='nav' className='smooth-edges'>
+					Nav
+				</div>
+				<div id='user-info' className='smooth-edges'>
+					User Info
+				</div>
+				<div id='education' className='smooth-edges'>
+					Education
+				</div>
+				<div id='pro-des' className='smooth-edges'>
+					Project description
+				</div>
+				<div id='tech-used' className='smooth-edges'>
+					Technologies used
+				</div>
+				<div id='tech-learned' className='smooth-edges'>
+					Technologies Learned
+				</div>
+				<div id='project-1' className='smooth-edges'>
+					Project 1
+				</div>
+				<div id='project-2' className='smooth-edges'>
+					{' '}
+					Project 2
+				</div>
+				<div id='project-3' className='smooth-edges'>
+					Project 3
+				</div>
+				<div id='project-viewer' className='smooth-edges'>
+					Project Viewer
+				</div>
+				<div id='hobby-selector' className='smooth-edges'>
+					Hobby Selector
+				</div>
+				<div id='hobby-des' className='smooth-edges'>
+					Hobby Description
+				</div>
+				<div id='contact-details' className='smooth-edges'>
+					Contact Details
+				</div>
+			</div>
 		</div>
 	);
 }
