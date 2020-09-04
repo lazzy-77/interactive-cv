48b5832777567fc21b611adb8e2ded3b33191def
lazzy-77 <45042801+lazzy-77@users.noreply.github.com>
2020-09-05 00:50:24 +0100

    major updates

--- src/App.js
+++ src/App.js
@@ -1,51 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useSpring, animated } from 'react-spring';
+import { Spring } from 'react-spring/renderprops';
 
 function App() {
+	const iconStyle = [
+		{
+			backgroundImage: `url(${require('./images/html-5-icon.svg')})`,
+			backgroundPosition: 'center',
+			backgroundSize: '50%',
+			backgroundRepeat: 'no-repeat',
+		},
+		{
+			backgroundImage: `url(${require('./images/css-icon.svg')})`,
+			backgroundPosition: 'center',
+			backgroundSize: '50%',
+			backgroundRepeat: 'no-repeat',
+		},
+		{
+			backgroundImage: `url(${require('./images/java-icon.svg')})`,
+			backgroundPosition: 'center',
+			backgroundSize: '50%',
+			backgroundRepeat: 'no-repeat',
+		},
+		{
+			backgroundImage: `url(${require('./images/sql-icon.svg')})`,
+			backgroundPosition: 'center',
+			backgroundSize: '50%',
+			backgroundRepeat: 'no-repeat',
+		},
+		{
+			backgroundImage: `url(${require('./images/javascript-icon.svg')})`,
+			backgroundPosition: 'center',
+			backgroundSize: '50%',
+			backgroundRepeat: 'no-repeat',
+		},
+		{
+			backgroundImage: `url(${require('./images/react-icon.svg')})`,
+			backgroundPosition: 'center',
+			backgroundSize: '65%',
+			backgroundRepeat: 'no-repeat',
+		},
+	];
+
+	const springProps = useSpring({
+		from: { opacity: 0 },
+		to: { opacity: 1 },
+	});
+
+	console.log(springProps);
 	return (
-		<div className='container-padding'>
-			<div className='container'>
-				<div id='nav' className='smooth-edges'>
-					Nav
+		<div className='container'>
+			<div id='nav' className=''>
+				Nav
+			</div>
+			<div id='user-info' className=''>
+				User Info
+			</div>
+			<div id='education' className=''>
+				Education
+			</div>
+			<div id='pro-des' className=''>
+				Project description
+			</div>
+			<div id='tech-used' className=''>
+				Technologies used
+			</div>
+			<div id='tech-learned' className=''>
+				<div className='tech-learned-container'>
+					<div className='html-icon' style={iconStyle[0]}></div>
+					<div className='html-bar'>2</div>
+					<div className='css-icon' style={iconStyle[1]}></div>
+					<div className='css-bar'>4</div>
+					<div className='java-icon' style={iconStyle[2]}></div>
+					<div className='java-bar'>6</div>
+					<div className='sql-icon' style={iconStyle[3]}></div>
+					<div className='sql-bar'>8</div>
+					<div className='javascript-icon' style={iconStyle[4]}></div>
+					<div className='javascript-bar'>10</div>
+					<div className='react-icon' style={iconStyle[5]}></div>
+					<div className='react-bar'>12</div>
 				</div>
-				<div id='user-info' className='smooth-edges'>
-					User Info
-				</div>
-				<div id='education' className='smooth-edges'>
-					Education
-				</div>
-				<div id='pro-des' className='smooth-edges'>
-					Project description
-				</div>
-				<div id='tech-used' className='smooth-edges'>
-					Technologies used
-				</div>
-				<div id='tech-learned' className='smooth-edges'>
-					Technologies Learned
-				</div>
-				<div id='project-1' className='smooth-edges'>
-					Project 1
-				</div>
-				<div id='project-2' className='smooth-edges'>
-					{' '}
-					Project 2
-				</div>
-				<div id='project-3' className='smooth-edges'>
-					Project 3
-				</div>
-				<div id='project-viewer' className='smooth-edges'>
-					Project Viewer
-				</div>
-				<div id='hobby-selector' className='smooth-edges'>
-					Hobby Selector
-				</div>
-				<div id='hobby-des' className='smooth-edges'>
-					Hobby Description
-				</div>
-				<div id='contact-details' className='smooth-edges'>
-					Contact Details
-				</div>
+			</div>
+			<div id='project-1' className=''>
+				Project 1
+			</div>
+			<div id='project-2' className=''>
+				{' '}
+				Project 2
+			</div>
+			<div id='project-3' className=''>
+				Project 3
+			</div>
+			<div id='project-viewer' className=''>
+				Project Viewer
+			</div>
+			<div id='hobby-selector' className=''>
+				Hobby Selector
+			</div>
+			<div id='hobby-des' className=''>
+				Hobby Description
+			</div>
+			<div id='contact-details' className=''>
+				Contact Details
 			</div>
 		</div>
 	);
